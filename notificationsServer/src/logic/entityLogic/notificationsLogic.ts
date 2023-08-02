ff60f17e79a80a44264fba08ec7003f75f8e18c1
MyName <you@example.com>
2023-08-02 17:05:13 +0300

    use Promise.all()

--- notificationsServer/src/logic/entityLogic/notificationsLogic.ts
+++ notificationsServer/src/logic/entityLogic/notificationsLogic.ts
@@ -16,28 +16,24 @@ export function createNotificationsLogic(dataProvider: iDataProvider) {
     }
 
     const notificationTrigger = async () => {
-        try {
-            const tasks = await getAll();
-            const date = (new Date()).getTime();
-            tasks.forEach((task) => {
-                if (task.deadline < date && !task.isExpired) {
+            const tasks = await getAll()
+            const date = (new Date()).getTime()
+            const promises = tasks.map(async (task) => {
+                if(task.deadline <= date && !task.isExpired) {
                     try {
-                        sendNotification()
-                        console.log('taskName: ', task.name)
-                        console.log('task.isExpired: ', task.isExpired)
+                        await sendNotification()
                     } catch(e) {
                         throw new Error(`sendNotification does not work: ${e.message}`)
                     }
+
                     try {
-                        console.log('is Exoired happening')
-                        dataProvider.NotificationDataProvider.updateIsExpired(task._id)
+                        await dataProvider.NotificationDataProvider.updateIsExpired(task._id);
                     } catch(e) {
-                        console.error('isExpired cant be updated in DB: ',(e))
+                        throw new Error(`isExpired cant be updated in DB: ${e}`);
                     }
-                }
+                } 
             })
-        } catch(e) {
-            console.error('All tasks cannot be fetched: ', (e))
+            
+            await Promise.all(promises)
         }
-    }
 }
